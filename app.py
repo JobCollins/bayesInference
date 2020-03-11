2a1abf98c4081da7a1b8cc30ab14f8f029fbec7b
Job Collins <job.collins@sunesiskenya.com>
2020-03-11 12:57:18 +0300

    Tried to display graph but failed

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 from random import randint
 from time import strftime
-from flask import Flask, render_template, flash, request
+from flask import Flask, render_template, flash, request, Response
 from wtforms import Form, TextField, TextAreaField, validators, StringField, SubmitField
+
+# plotting
+import json
+import plotly
+import plotly.graph_objs as go
 
 DEBUG = True
 app = Flask(__name__)
@@ -19,6 +24,7 @@ class BayesForm(Form):
 def bayes():
     form = BayesForm(request.form)
     prob = None
+    global specificity, sensitivity, prevalence, threshold
     threshold = None
     #print(form.errors)
     if request.method == 'POST':
@@ -34,12 +40,17 @@ def bayes():
             """
             Computes the posterior using Bayes' rule
             """
-            p_user = float(prevalence_v)
-            p_non_user = 1-float(prevalence_v)
-            p_pos_user = float(sensitivity_v)
-            p_neg_user = float(specificity_v)
-            p_pos_non_user = 1-float(specificity_v)
+            specificity = float(specificity_v)
+            sensitivity = float(sensitivity_v)
+            prevalence = float(prevalence_v)
             threshold = float(threshold_v)
+
+            p_user = prevalence
+            p_non_user = 1-prevalence
+            p_pos_user = sensitivity
+            p_neg_user = specificity
+            p_pos_non_user = 1-float(specificity_v)
+            
             
             num = p_pos_user*p_user
             den = p_pos_user*p_user+p_pos_non_user*p_non_user
@@ -57,6 +68,50 @@ def bayes():
             flash('Error: All Fields are Required')
 
     return render_template('index.html', form=form, value=prob, threshold=threshold)
+
+def posterior(specificity, sensitivity, prevalence, threshold):
+    
+    p_user = prevalence
+    p_non_user = 1-prevalence
+    p_pos_user = sensitivity
+    p_neg_user = specificity
+    p_pos_non_user = 1-specificity
+    
+    
+    num = p_pos_user*p_user
+    den = p_pos_user*p_user+p_pos_non_user*p_non_user
+    
+    
+    prob = num/den
+
+    return prob
+
+@app.route("/", methods=['GET', 'POST'])
+def plot_png():
+    # logic        
+    bar = create_graph()
+    
+    return render_template('index.html', plot=bar)
+
+def create_graph():
+
+    probs=[]
+    prevs=[]
+    for prev in [i*0.001 for i in range(1,51,2)]:
+        prevs.append(prev*100)
+        prob = posterior(sensitivity=sensitivity,specificity=specificity,prevalence=prev, threshold=threshold)
+        probs.append(prob)
+
+    data = [
+        go.Bar(
+            x=prevs,
+            y=probs
+        )
+    ]
+
+    graph = json.dumps(data, cls=plotly.utils.PlotlyJSONEncoder)
+
+    return graph
 
 if __name__ == "__main__":
     app.run(port=3000, debug=True)
