5c32b9ed0c78fcfd0658583ec2463cd5763602aa
Job Collins <job.collins@sunesiskenya.com>
2020-03-12 10:03:17 +0300

    Added the sensitivity explanation and graph

--- app.py
+++ app.py
@@ -27,6 +27,7 @@ def bayes():
     global specificity, sensitivity, prevalence, threshold
     threshold = None
     bar = None
+    sens_line = None
     #print(form.errors)
     if request.method == 'POST':
 
@@ -66,12 +67,13 @@ def bayes():
                 print("The test-taker may not be an user")
             
             bar = create_graph()
+            sens_line = create_sens_graph()
 
         else:
             flash('Error: All Fields are Required')
             
 
-    return render_template('index.html', form=form, value=prob, threshold=threshold, plot=bar)
+    return render_template('index.html', form=form, value=prob, threshold=threshold, plot=bar, sens=sens_line)
 
 def posterior(specificity, sensitivity, prevalence, threshold):
     
@@ -125,6 +127,56 @@ def create_graph():
         xaxis=go.layout.XAxis(
             title=go.layout.xaxis.Title(
                 text='Prevalence(%)',
+                font=dict(
+                    family='Roboto',
+                    size=18,
+                    color='#7f7f7f'
+                )
+            )
+        ),
+        yaxis=go.layout.YAxis(
+            title=go.layout.yaxis.Title(
+                text='Probability of a True Positive',
+                font=dict(
+                    family='Roboto',
+                    size=18,
+                    color='#7f7f7f'
+                )
+            )
+        )
+    )
+    fig = go.Figure(data=data, layout=layout)
+    
+    graph = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+    
+    return graph
+
+def create_sens_graph():
+
+    probs=[]
+    sens=[]
+    for sen in [i*0.001+0.95 for i in range(1,50,2)]:
+        sens.append(sen)
+        prob = posterior(sensitivity=sen,specificity=specificity,prevalence=prevalence, threshold=threshold)
+        probs.append(prob)
+    
+    data = [
+        go.Scatter(
+            x=sens,
+            y=probs,
+            mode='lines+markers'
+        )
+    ]
+
+    layout = go.Layout(
+        title=go.layout.Title(
+            text='Probability of True Positive with Test Sensitivity',
+            xref='paper',
+            x=0
+        ),
+        xaxis=go.layout.XAxis(
+            title=go.layout.xaxis.Title(
+                text='Sensitivity(%)',
                 font=dict(
                     family='Roboto',
                     size=18,
